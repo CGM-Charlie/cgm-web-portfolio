bf788b4c699d6c62f063d3ee2f2dfe3a6470d1fd
Carlos Mora <58601922+CGM-Charlie@users.noreply.github.com>
2022-06-20 22:23:07 -0500

    Setup Chakra UI (#4)
    - Fixes #3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+
+import Theme from './Theme';
+import Home from './components/Home';
 
 function App() {
     return (
-        <Routes>
-            <Route path="/home" element={<>homepage</>} />
-            <Route path="*" element={<Navigate to="/home" replace />} />
-        </Routes>
+        <ChakraProvider theme={Theme}>
+            <ColorModeScript initialColorMode={Theme.config.initialColorMode} />
+            <Routes>
+                <Route path="/home" element={<Home></Home>} />
+                <Route path="*" element={<Navigate to="/home" replace />} />
+            </Routes>
+        </ChakraProvider>
     );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navbar from './Navbar';
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+            <div>Pagina de Inicio</div>
+        </div>
+    );
+}
+
+export default Home;
